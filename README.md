f39884af00a5077f231d9367d0f623cdb9ce81a0
Dean Cook <deanrobertcook@gmail.com>
2023-03-04 13:30:50 +0100

    Finish first rough version

---
+++ README.md
@@ -0,0 +1,9 @@
+### Scan receipts into Google drive folder (receipt-ocr)
+- Don't use any picture correction
+- Try to ensure that the picture is straight
+- Use good lighting, try to flatten the receipt as much as possible
+- Share folder, or do scans by myself
+
+### Plan for processing:
+- Create CSV with date, total, merchant, purchaser, lines with euro amounts, URL to original receipt
+- 
--- asprise.py
+++ asprise.py
@@ -1,21 +1,26 @@
+import sys
 import requests
 import os
 import json
+import re
 
-DEBUG = True
+DEBUG = False
+DB_PATH = 'cache/db.json'
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 
 BASE_URL = 'https://ocr.asprise.com/api/v1/receipt'  # Receipt OCR API endpoint
-# // Modify this to use your own file if necessary
-imageFile = "/Users/deancook/Meine Ablage/receipt-scanner/230223-BC.pdf"
-
 
 def cached_request_receipt_scan(filename):
   cache = os.path.join('cache', os.path.basename(filename) + ".json")
+
   if os.path.isfile(cache):
     if DEBUG:
       print(f"{cache} found, reading from file")
     with open(cache) as f:
-      return json.load(f)
+      return (json.load(f), cache)
+    
   if DEBUG:
     print(f"{cache} not found, fetching")
   response = requests.post(
@@ -30,8 +35,55 @@ def cached_request_receipt_scan(filename):
   os.makedirs(os.path.dirname(cache), exist_ok=True)
   with open(cache, 'w') as f:
     json.dump(js, f, indent=4, separators=(",", ": "))
-  return js
+  return (js, cache)
+
+receipts = sorted(os.listdir("receipts"), key=lambda f: os.path.getctime(os.path.join("receipts", f)))
+# receipts = ['Double_20230304-1220.pdf']
+
+pattern = r"^(.*(\d+[\,|\.]\d+).*)$"   
+text_pattern = r"\D" # Match any non-digit character 
+
+with open(DB_PATH) as f:
+  db = json.load(f)
+
+for i, f in enumerate(receipts):
+  (js, cache) = cached_request_receipt_scan(os.path.join("receipts", f))
+  if 'message' in js:
+    print("Rate limit hit, stopping")
+    os.remove(cache)
+    exit(0)
+  
+  ocr_text = js['receipts'][0]['ocr_text']
+
+  receipt = {
+    "date": js['receipts'][0]['date'],
+    "total": js['receipts'][0]['total'],
+    "merchant": js['receipts'][0]['merchant_name'],
+    "purchaser": f.split('_')[0],
+    "file": f,
+  }
+  
+  matches = re.findall(pattern, ocr_text, re.MULTILINE)
+
+  items = []
+  for match in matches:
+    line_parts = match[0].strip().split("  ")
+    is_text = lambda p: bool(re.search(text_pattern, p))
+    item_name = list(filter(is_text, line_parts))[0].strip()
+
+    price = match[1].strip()
+    cents = int(price.replace(",", "").replace(".", ""))
+    items.append([item_name, cents])
+
+  receipt['items'] = items
+
+  if f in db:
+    if db[f]['items'] != receipt['items']:
+      print("Items changed for", f, file=sys.stderr)
+  else:
+    db[f] = receipt
+    with open(DB_PATH, 'w') as f:
+      json.dump(db, f, indent=4, separators=(",", ": "))
 
 
-for f in os.listdir("receipts"):
-  cached_request_receipt_scan('receipts/' + f)
+  
